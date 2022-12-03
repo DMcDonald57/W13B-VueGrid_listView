3b86435f69e4ff40681f692af5f23ec7d8ec0fb1
DMcDonald57 <d.mcdonald579@gmail.com>
2022-12-02 19:42:21 -0700

    pre assignment work.  Created Vue file and component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TweetProject/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TweetProject from '@/components/TweetProject.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TweetProject
   }
 }
 </script>
---
+++ src/components/TweetProject.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "TweetProject"
+    }
+</script>
+
+<style scoped>
+
+</style>
