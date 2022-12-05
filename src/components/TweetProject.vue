29b196e5554795fc1bec8c1446b8626687a2c285
DMcDonald57 <d.mcdonald579@gmail.com>
2022-12-04 20:53:14 -0700

    I cant even figure our how to get the info on the page

--- src/components/TweetProject.vue
+++ src/components/TweetProject.vue
@@ -1,12 +1,64 @@
 <template>
     <div>
-
+        <button @click="toggleGrid">Grid View</button>
+        
+            <!-- <p>${"tweets.tweet"}</p>
+            <p>{{twitterName}}</p>
+            <p>{{twitterDate}}</p> -->
     </div>
 </template>
 
 <script>
     export default {
-        name : "TweetProject"
+        name : "TweetProject",
+        data(){
+            return {
+                tweets :[
+                    {
+                        tweet: "Nice one",
+                        name: "Dwayne",
+                        date: "04 Dec 2022"
+                    },
+                    {
+                        tweet: "Not bad at all",
+                        name: "Adam",
+                        date: "25 Nov 2022"
+                    },
+                    {
+                        tweet: "Really nice work",
+                        name: "Mahi",
+                        date: "02 Dec 2022"
+                    },
+                    {
+                        tweet: "That is looking good",
+                        name: "Victor",
+                        date: "02 Dec 2022"
+                    },
+                    {
+                        tweet: "What does it all mean",
+                        name: "Rebecca",
+                        date: "22 Nov 2022"
+                    },
+                    {
+                        tweet: "Not to sure about that",
+                        name: "Tyler",
+                        date: "4 Dec 2022"
+                    }
+                ]
+            }
+        },
+        methods: {
+        },
+        mounted () {
+            // v-for="(twitter, index) in tweets"
+            // :key="index"
+            // :tweet="twitter.tweet"
+            // :name="twitter.name"
+            // :date="twitter.date"
+
+            
+        }
+        
     }
 </script>
 
